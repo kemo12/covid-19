855c740aa5c3eb3b8168fd21f3d59fadda69e38e
kemo12 <dodkamel919@gmail.com>
2020-12-06 11:46:27 +0300

    6/12/2020 part 1"

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import Loading from "../../components/Map/components/Loading";
+import CovidMap from "../../components/Map/components/CovidMap";
+import LoadCountriesTask from "../..//components/Map/tasks/LoadCountriesTask";
+import Legend from "../../components/Map/components/Legend";
+import legendItems from "../../components/Map/entities/LegendItems";
+
 const Map = () => {
+  const [countries, setCountries] = useState([]);
+
+  const legendItemsReverse = [...legendItems].reverse();
+
+  const load = () => {
+    console.log("load");
+    const loadCountriesTask = new LoadCountriesTask();
+    loadCountriesTask.load((countries) => setCountries(countries));
+  };
+
+  useEffect(load, []);
+
   return (
     <div>
-      <p>Map</p>
+      {countries.length === 0 ? (
+        <Loading />
+      ) : (
+        <div>
+          <CovidMap countries={countries} />
+          <Legend legendItems={legendItemsReverse} />
+        </div>
+      )}
     </div>
   );
 };
+
 export default Map;
